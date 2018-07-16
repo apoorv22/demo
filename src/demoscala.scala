272b42dcb82a4325beac93b428f6e89e7d1321cd
Abhishek <abhishekprsetty@gmail.com>
2018-07-16 19:38:17 +0530

    updated dir and files

---
+++ src/demoscala.scala
@@ -0,0 +1,12 @@
+
+object demoscala {
+  def main(args: Array[String]) {
+    var counter: Int=0;
+
+    for(counter <- 1 until 101)
+      print(counter + " ");
+
+    // to print new line
+    println();
+  }
+}
